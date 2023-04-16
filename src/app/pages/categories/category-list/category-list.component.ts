b2e8c5c2b1eb52789377453cf7a119a1dfdfd982
Gleison Freitas <gleison8405@gmail.com>
2023-04-16 15:50:15 -0300

    improving CategoryList with BaseResourceListComponent

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+import { BaseResourceListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
 import { Category } from '../shared/category.model';
 import { CategoryService } from '../shared/category.service';
 
@@ -8,31 +9,10 @@ import { CategoryService } from '../shared/category.service';
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent implements OnInit {
+export class CategoryListComponent extends BaseResourceListComponent<Category> {
 
-  categories: Category[] = [];
-
-  constructor( private categoryService: CategoryService ) { }
-
-  ngOnInit() {
-    this.categoryService.getAll().subscribe(
-      categories => this.categories = categories,
-      error => {
-        alert('Loading list error');
-        console.log(error);
-      }
-    );
-  }
-
-  deleteCategory(category: Category) {
-    const mustDelete = confirm(`Do you want delete the category ${category.name}?`);
-
-    if (mustDelete) {
-      this.categoryService.delete(category.id).subscribe(
-        () => this.categories = this.categories.filter(element => element !== category),
-        () => alert('Trying delete error')
-      );
-    }
+  constructor( private categoryService: CategoryService ) {
+    super(categoryService);
   }
 
 }
